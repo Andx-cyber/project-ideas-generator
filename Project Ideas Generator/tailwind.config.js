0762d921a1b3bea7e69bd09caaecc764cb0a4b28
Andx-cyber <dachraouiadem628@gmail.com>
2025-05-09 18:12:05 +0100

    Add files via upload

---
+++ Project Ideas Generator/tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+      animation: {
+        'bounce-slow': 'bounce 3s linear infinite',
+      }
+    }
+  }
+} 
